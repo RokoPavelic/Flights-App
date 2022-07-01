58e4517d877fb4022a556ed7e3266a6677e5d398
Antonello Fois <antonello.fois1381@gmail.com>
2022-07-01 10:39:46 +0200

    spinner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import DepartureDropdown from './components/DepartureDropdown';
 import DestinationDropdown from './components/DestiantionDropdown';
+import LoadingSpinner from './components/LoadingSpinner';
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className="App">
      <DepartureDropdown />
      <DestinationDropdown />
+     <LoadingSpinner />
+
     </div>
   );
 }
