340265453597aac26d61e7067163c3fb204cd69c
Marian <marsaldana@Marians-MacBook-Air.local>
2022-07-01 12:39:53 +0200

    minor css to flight

--- src/Flight.jsx
+++ src/Flight.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { DateTime } from "luxon";
+import './Flight.css';
 
 const Flight = ({ flight }) => {
   const { cityFrom, cityTo, aTime, dTime, price } = flight;
   return (
-    <div>
-      <p> From:{cityFrom}</p>
-      <p> To:{cityTo}</p>
+    <div className="container">
+      <p> From: {cityFrom}</p>
+      <p> To: {cityTo}</p>
       <p>
-        Departure time:{DateTime.fromMillis(dTime * 1000).toFormat("hh:mm")}
+        Departure time: {DateTime.fromMillis(dTime * 1000).toFormat("hh:mm")}
       </p>
-      <p>Arivale time:{DateTime.fromMillis(aTime * 1000).toFormat("hh:mm")}</p>
-      <p>Price:{price}</p>
+      <p>Arivale time: {DateTime.fromMillis(aTime * 1000).toFormat("hh:mm")}</p>
+      <p>Price: ${price}</p>
     </div>
   );
 };
