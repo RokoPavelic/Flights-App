deeff8c9f598c476c6a5cdc5ed1c0417c08c32e3
RokoPavelic <rokoprivatno@gmail.com>
2022-07-01 11:22:11 +0200

    dropdowns added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-import './App.css';
+import "./App.css";
 import Flight from "./Flight";
-import DepartureDropdown from './components/DepartureDropdown';
-import DestinationDropdown from './components/DestiantionDropdown';
-
+import DepartureDropdown from "./components/DepartureDropdown";
+import DestinationDropdown from "./components/DestiantionDropdown";
 
 function App() {
   const [flights, setFlights] = useState();
-  const url =
-    "https://api.skypicker.com/flights?fly_from=PRG&fly_to=VLC&partner=data4youcbp202106";
+  const [departure, setDeparture] = useState("PRG");
+  const [destination, setDestination] = useState("VLC");
+  const url = `https://api.skypicker.com/flights?fly_from=${departure}&fly_to=${destination}&partner=data4youcbp202106`;
 
   async function fetchData(url) {
     const response = await fetch(url);
@@ -19,17 +19,22 @@ function App() {
 
   useEffect(() => {
     fetchData(url);
-  }, []);
+  }, [departure, destination]);
 
   console.log(flights);
   return (
     <div className="App">
+      <DepartureDropdown setDeparture={setDeparture} departure={departure} />
+      <DestinationDropdown
+        setDestination={setDestination}
+        destination={destination}
+      />
+
       {flights ? (
-        flights.data.map((flight, i) => <Flight flight={flight} />)
+        flights?.data?.map((flight, i) => <Flight key={i} flight={flight} />)
       ) : (
         <p>Loading...</p>
       )}
-    
     </div>
   );
 }
--- src/components/DestiantionDropdown.jsx
+++ src/components/DestiantionDropdown.jsx
@@ -1,18 +1,18 @@
-import Form from 'react-bootstrap/Form'; 
+import Form from "react-bootstrap/Form";
 
-
-
-export default function DestinationDropdown() {
-
- return(
-        <Form.Select aria-label="Departure Origin">
-            <option>Select a destination</option>
-            <option value="1">Athens</option>
-            <option value="2">Barcellona</option>
-            <option value="3">Madrid</option>
-            <option value="4">Milano</option>
-            <option value="5">Valencia</option>
-        </Form.Select>
-        )
-       
+export default function DestinationDropdown({ setDestination, destination }) {
+  return (
+    <Form.Select
+      aria-label="Departure Origin"
+      onChange={(e) => setDestination(e.target.value)}
+      value={destination}
+    >
+      <option>Select a destination</option>
+      <option value="ATH">Athens</option>
+      <option value="BCN">Barcellona</option>
+      <option value="MAD">Madrid</option>
+      <option value="MXP">Milano</option>
+      <option value="VLC">Valencia</option>
+    </Form.Select>
+  );
 }
